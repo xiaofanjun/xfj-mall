06cd46c53b6fd2ae9e49f8ab5e73ea10df0f3f23
ZQ <519996418@qq.com>
2020-01-06 20:45:46 +0800

    public.js 添加注释

--- xfj-front/src/api/public.js
+++ xfj-front/src/api/public.js
@@ -1,8 +1,15 @@
+/**
+ * @Author ZQ
+ * @Description 前后端交互方式公用文件
+ * @Date 2020/1/5 18:13
+ **/
 import axios from 'axios'
+
 axios.defaults.timeout = 10000
+// 默认格式
 axios.defaults.headers.post['Content-Type'] = 'application/x-www=form-urlencoded'
 export default {
-  fetchGet (url, params = {}) {
+  fetchGet: function (url, params = {}) {
     return new Promise((resolve, reject) => {
       axios.get(url, params).then(res => {
         resolve(res.data)
@@ -11,7 +18,7 @@ export default {
       })
     })
   },
-  fetchPost (url, params = {}) {
+  fetchPost: function (url, params = {}) {
     return new Promise((resolve, reject) => {
       axios.post(url, params).then(res => {
         resolve(res.data)
@@ -20,7 +27,7 @@ export default {
       })
     })
   },
-  fetchPut (url, params = {}) {
+  fetchPut: function (url, params = {}) {
     return new Promise((resolve, reject) => {
       axios.put(url, params).then(res => {
         resolve(res.data)
@@ -29,7 +36,7 @@ export default {
       })
     })
   },
-  fetchDelete (url, params = {}) {
+  fetchDelete: function (url, params = {}) {
     return new Promise((resolve, reject) => {
       axios.delete(url, params).then(res => {
         resolve(res.data)
